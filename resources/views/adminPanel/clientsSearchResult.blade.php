26037f46ac548a6115b58eaa02b47f3a6cfb8e18
Unknown <alexmelkov@mail.ru>
2018-07-02 20:37:53 +0300

    Исправление css в поиске пользователей

--- resources/views/adminPanel/clientsSearchResult.blade.php
+++ resources/views/adminPanel/clientsSearchResult.blade.php
@@ -1,17 +1,17 @@
 @foreach($arResult as $key=>$user)
-    <div class="row clientsTableRow" id="clientsTableRow-{{$key}}">
+    <div class="row clientsTableRow adminPanelTableRow" id="clientsTableRow-{{$key}}">
         <div class="col-1 clientsTableChecker">
             <div class="form-check">
                 <input class="form-check-input clientsTableChecker" type="checkbox" value=""
                        id="clientsTableChecker-{{$key}}">
             </div>
         </div>
-        <div class="col-1 clientsTableNickName">{{ $user['nickName'] }}</div>
-        <div class="col-2 clientsTableFirstName">{{ $user['first_name'] }}</div>
-        <div class="col-2 clientsTableLastName">{{ $user['last_name'] }}</div>
-        <div class="col-2 clientsTablePhone">{{ $user['phone_number'] }}</div>
-        <div class="col-2 clientsTableEmail">{{ $user['email'] }}</div>
-        <div class="col-2 clientsTableCity">{{ $user['city'] }}</div>
+        <div class="col-1 clientsTableNickName adminPanelTableCol">{{ $user['nickName'] }}</div>
+        <div class="col-2 clientsTableFirstName adminPanelTableCol">{{ $user['first_name'] }}</div>
+        <div class="col-2 clientsTableLastName adminPanelTableCol">{{ $user['last_name'] }}</div>
+        <div class="col-2 clientsTablePhone adminPanelTableCol">{{ $user['phone_number'] }}</div>
+        <div class="col-2 clientsTableEmail adminPanelTableCol">{{ $user['email'] }}</div>
+        <div class="col-2 clientsTableCity adminPanelTableCol">{{ $user['city'] }}</div>
     </div>
 @endforeach
 <script src="{{ asset('js/admin/client/clients.js') }}" defer></script>
